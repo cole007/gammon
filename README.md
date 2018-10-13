fb02cf2a0b66d1d8ad0834bd63d0731db34e93f4
Cole Henley <cole@ournameismud.co.uk>
2018-10-13 13:28:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Gammon
+=============
+
+The world needs a little less Tommy Robinson in it so why not install this Chrome Extension to replace any instance of Tommy Robinson with his birth name, 'Stephen Christopher Yaxley-Lennon'. 
+
+Image courtesy of Cédric Villain from the Noun Project.
---
+++ background.js
@@ -0,0 +1,32 @@
+walk(document.body);
+
+function walk(node)  
+{
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1: 
+		case 9:  
+		case 11: 
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling; 
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: 
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+	v = v.replace(/\Tommy Robinson\b/g, "Stephen Christopher Yaxley-Lennon")	
+	textNode.nodeValue = v;
+}
